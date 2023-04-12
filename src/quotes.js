f9ed64fa0bd315b6db8e121079caf7f1710d5111
Matt Mullins <matt.mullins@live.com>
2023-04-12 15:54:31 +0100

    Updated JS and Button

--- src/quotes.js
+++ src/quotes.js
@@ -1,25 +1,12 @@
-// class Constructor{
-//     constructor(text) {
-//         this.text = text
-//     }
-
-//     randomQuote() {
-//         this.text = this.text.toString().random[0];
-//     }
-// }
-
-function quoteGenerator(quotes) {
-    this.quotes = quotes;
-    this.randomQuote = function() {
-        const randomIndex = Math.floor(Math.random() * this.quotes.lenght);
-        return this.quotes[randomIndex];
-    }
-}
-
-const myQuoteGenerator = new quoteGenerator([
+const quotes = [
     "The greatest glory in living lies not in never falling, but in rising every time we fall.",
     "The way to get started is to quit talking and begin doing.",
     "If life were predictable it would cease to be life, and be without flavor.",
     "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
     "It does not matter how slowly you go as long as you do not stop."
-])
+]
+
+function newQuote() {
+    let randomNumber = Math.floor(Math.random() * (quotes.length));
+    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+}
